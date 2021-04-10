eb251ac966a8150d592c8857bd72fa70e434b406
= <=>
2021-04-10 17:41:56 -0500

    vectorized text

--- text_classification.py
+++ text_classification.py
@@ -14,3 +14,31 @@ dataset = "/home/prog/Desktop/Translator/aclImdb"
 dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
 train_dir = os.path.join(dataset_dir, 'train')
 
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory('aclImdb/train', batch_size=batch_size, validation_split=0.2, subset='training',seed=seed)
+raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory("aclImdb/train", batch_size=batch_size, validation_split=0.2, subset="validation", seed=seed)
+raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory("aclImbd/test", batch_size=batch_size)
+
+def custom_standardization(input_data):
+    lowercase = tf.strings.lower(input_data)
+    stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+    return tf.strings.regex_replace(stripped_html, '[%s]' % re.escape(string.punctuation), '')
+
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length
+)
+
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+def vectorize_text(text, label):
+    text = tf.expand_dims(text, -1)
+    return vectorize_layer(text), label
